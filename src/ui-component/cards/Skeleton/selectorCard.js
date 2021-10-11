b59a7b14dac3eb89bff7e27ffe33ec0a149ee0cd
EC2 Default User <ec2-user@ip-172-31-36-145.ap-southeast-2.compute.internal>
2021-10-11 05:39:46 +0000

    corrected focus lable color and added all 10 years

--- src/ui-component/cards/Skeleton/selectorCard.js
+++ src/ui-component/cards/Skeleton/selectorCard.js
@@ -77,9 +77,9 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     inputLabel: {
-        color: theme.palette.secondary.light,
-        '& .focused': {
-            color: '#fff'
+        color: theme.palette.primary.light,
+        '&.Mui-focused': {
+            color: theme.palette.secondary[200]
         }
     },
     MuiInputLabel: {
@@ -125,6 +125,41 @@ const SelectorCard = ({ isLoading }) => {
         {
             id: 3,
             name: 'Year3',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 4,
+            name: 'Year4',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 5,
+            name: 'Year5',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 6,
+            name: 'Year6',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 7,
+            name: 'Year7',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 8,
+            name: 'Year8',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 9,
+            name: 'Year9',
+            sesonalCondition: 'avg'
+        },
+        {
+            id: 10,
+            name: 'Year10',
             sesonalCondition: 'avg'
         }
     ]);
